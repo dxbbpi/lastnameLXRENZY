02e9f7595d4243dc7c5567a375efd6db95ea8b36
dxbbpi <olafz9zzz@gmail.com>
2025-04-19 16:32:15 +0700

    Add files via upload

---
+++ membersrp.js
@@ -0,0 +1,24 @@
+// member.js
+const members = [
+    { name: "Nonx Threethousand", fbId: "100092972023302#" },
+    { name: "Jox Jutistory", fbId: "lakksjslal.ksksiksks" },
+    { name: "Nevil Diamondonsnow", fbId: "Nevilwtfzera#" },
+    { name: "Midas Lxrenzy", fbId: "100065837666522#" },
+    { name: "Jaydase Vincere", fbId: "jaydase.vincere#" },
+    { name: "Hugo Vega", fbId: "61570465692953#" },
+    { name: "eiei dadasdas ", fbId: "61570465692953#" },
+    { name: "Felix Lxrenzy", fbId: "61553664876339#" }
+  ];
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const selectEl = document.getElementById("group");
+    if (selectEl) {
+      members.forEach(member => {
+        const option = document.createElement("option");
+        option.value = member.name;
+        option.textContent = member.name;
+        selectEl.appendChild(option);
+      });
+    }
+  });
+  
