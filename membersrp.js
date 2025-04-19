4766d9c0410657887deb02d75e31791550c4c946
dxbbpi <olafz9zzz@gmail.com>
2025-04-19 16:31:52 +0700

    Delete membersrp.js

--- membersrp.js
+++
@@ -1,9 +0,0 @@
-const members = [
-    { "name": "Nonx Threethousand", "fbId": "100092972023302#" },
-    { "name": "Jox Jutistory", "fbId": "lakksjslal.ksksiksks" },
-    { "name": "Nevil Diamondonsnow", "fbId": "Nevilwtfzera#" },
-    { "name": "Midas Lxrenzy", "fbId": "100065837666522#" },
-    { "name": "Jaydase Vincere", "fbId": "jaydase.vincere#" },
-    { "name": "Hugo Vega", "fbId": "61570465692953#" },
-    { "name": "Felix Lxrenzy", "fbId": "61553664876339#" }
-];
